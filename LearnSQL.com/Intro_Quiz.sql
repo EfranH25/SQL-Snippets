ff36b3fbcedb8f1b691283a497506834f7b14f5b
EfranH25 <60274368+EfranH25@users.noreply.github.com>
2020-10-25 15:22:02 -0400

    Update Intro_Quiz.sql

--- LearnSQL.com/Intro_Quiz.sql
+++ LearnSQL.com/Intro_Quiz.sql
@@ -1,67 +1,68 @@
 # Find all recipes, but select only name, rating, and cook_time for recipe. Sort records in order of descending rating,
 # with names of equally rated recipes in alphabetical (ascending) order.
-
-select name, rating, cook_time
-from recipe
-order by rating desc, name;
+SELECT name, rating, cook_time
+FROM recipe
+ORDER BY rating DESC, name;
 
 # For each ingredient type, find the number of ingredients. Name the columns type and number_of_ingredients.
-select type, count(*) as number_of_ingredients
-from ingredient
-group by type;
+SELECT type, count(*) AS number_of_ingredients
+FROM ingredient
+GROUP BY type;
 
 # The preparation of recipes takes some time. Find the shortest and longest cooking times. In the result table, display
 # the shortest cooking time (name column as min_time), and the longest cooking time (name column as max_time).
 
-select min(cook_time) as min_time , max(cook_time) as max_time
-from recipe asw
+SELECT min(cook_time) AS min_time, max(cook_time) AS max_time
+FROM recipe asw
 
 # How many recipes are assigned to each preference rating?
 # Only select ratings with the number of recipes higher than 1. Name the columns rating and number_of_recipes.
-select rating, count(*) as number_of_recipes
-from recipe
-group by rating
-having count(*)>1;
+SELECT rating, count(*) AS number_of_recipes
+FROM recipe
+GROUP BY rating
+HAVING count(*) > 1;
 
 # Each recipe contains a different number of ingredients. Find the number of ingredients in each recipe. Select only
 # those recipes which have fewer than 5 ingredients. Display the recipe name and number of ingredients
 # (name the column number_of_ingredients).
-select x.name, y.number_of_ingredients
-from recipe x
-join
-(
-select recipe_id, count(*) as number_of_ingredients
-from recipe_ingredients
-group by recipe_id
-having count(*) < 5
-  ) y
-  on x.id = y.recipe_id;
+SELECT x.name, y.number_of_ingredients
+FROM recipe x
+	     JOIN
+     (
+	     SELECT recipe_id, count(*) AS number_of_ingredients
+	     FROM recipe_ingredients
+	     GROUP BY recipe_id
+	     HAVING count(*) < 5
+     ) y
+     ON x.id = y.recipe_id;
 
 # A lot of people don't have much time to cook. Get the recipes which have cooking times below average for all recipes
 # in the recipe table. Return the names and cooking times for those recipes.
-select name, cook_time
-from recipe
-where cook_time < (
-select avg(cook_time)
-  from recipe
+SELECT name, cook_time
+FROM recipe
+WHERE cook_time < (
+	SELECT avg(cook_time)
+	FROM recipe
 );
 
 # Retrieve ids of ingredients which are not included in table recipe_ingredients.
-select id
-from ingredient x
-where x.id not in (
-select ingredient_id
-  from recipe_ingredients
+SELECT id
+FROM ingredient x
+WHERE x.id NOT IN (
+	SELECT ingredient_id
+	FROM recipe_ingredients
 );
 
 # A lot of people like sweets very much. We're willing to bet you do, too! Which recipes include sweets? Get names of
 # recipes which include at least one ingredient of type 'sweets', but whose ingredient name is not 'sugar'.
-select x.name
-from recipe x
-join
-(select recipe_id
-from recipe_ingredients
-where ingredient_id in (
-select id from ingredient
-where type = 'sweets' and name != 'sugar')) y
-on x.id = y.recipe_id;
+SELECT x.name
+FROM recipe x
+	     JOIN
+     (SELECT recipe_id
+      FROM recipe_ingredients
+      WHERE ingredient_id IN (
+	      SELECT id
+	      FROM ingredient
+	      WHERE type = 'sweets'
+		    AND name != 'sugar')) y
+     ON x.id = y.recipe_id;
